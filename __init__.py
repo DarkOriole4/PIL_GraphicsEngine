9bb2012a8d8c4d9f8db0b54e5e2c4afda8fdbf37
DarkOriole4 <105799574+DarkOriole4@users.noreply.github.com>
2023-06-13 00:05:27 +0200

    added init file

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import Image
+
+render_res = 64
+upscale_res = 862
+
+## Window  Settings
+root = Tk()
+# Create a frame
+app = Frame(root, bg="white")
+app.grid()
+root.resizable(False, False)
+# Create a label in the frame
+lmain = Label(app)
+lmain.grid()
