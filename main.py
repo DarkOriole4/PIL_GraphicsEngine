9aa35d1af2cbace02259df867a6a72419624d2f7
DarkOriole4 <105799574+DarkOriole4@users.noreply.github.com>
2023-06-14 16:31:10 +0200

    Modified the current scene

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from engine_functions import draw_line, import_model, draw_wireframe
+from engine_functions import draw_line, import_model, draw_wireframe, rotate_mesh
 from __init__ import render_res, upscale_res, root, lmain
 from tkinter import *
 from PIL import ImageTk, Image, ImageOps, ImageDraw, ImageFont
 from itertools import product
 import time
 import numpy
+from math import radians
 
 
 def render_frame(frame):
     start = time.time_ns()
     
     #CREATE BLACK CANVAS (blanking interval)
-    img = Image.new("RGB", size=(render_res, render_res), color=(0, 0, 0))
+    img = Image.new("RGB", size=(render_res, render_res), color=(0, 100, 100))
 
     #RENDER
-    px_list = range(render_res)
-    for x, y in product(px_list, px_list):
-        color = (int(x*(255/render_res)), int(y*(255/render_res)), 255)
-        img.putpixel((x, y), color)
+    # px_list = range(render_res)
+    # for x, y in product(px_list, px_list):
+    #     color = (int(x*(255/render_res)), int(y*(255/render_res)), 255)
+    #     img.putpixel((x, y), color)
         
     # ##ANIMATE TEST POLYGON
     # top line is at y_pos 33
@@ -35,7 +36,8 @@ def render_frame(frame):
     # img.putpixel([29, y_pos], (0, 0, 0))
 
     ## DRAW WIREFRAME
-    draw_wireframe(img, vertex_table, edge_table, (255, 255, 255))
+    rotated_table = rotate_mesh(vertex_table, radians(frame))
+    draw_wireframe(img, rotated_table, edge_table, (40, 255, 125))
 
     #UPSCALE
     factor = upscale_res / render_res
@@ -47,8 +49,8 @@ def render_frame(frame):
     end = time.time_ns()
     frame_time = (end-start)/1000000
     fps = 1000 / frame_time
-    draw.text((5,5), 'frame_time: %dms\nfps: %d' % (frame_time, fps), fill=(255, 255, 255), font=font, spacing=5, align='left')
-    #draw.text((5, upscale_res-20), 'frame: %d' % (frame), fill=(255, 255, 255), font=font)
+    draw.text((5,5), 'frame_time: %dms\nfps: %d' % (frame_time, fps), fill=(255, 255, 255), font=font, spacing=5, align='left') # displays frame time and fps
+    #draw.text((5, upscale_res-20), 'frame: %d' % (frame), fill=(255, 255, 255), font=font) # displays frame count
     
     return img
 
@@ -64,7 +66,7 @@ def refresh_screen():
     
 
 if __name__ == "__main__":
-    vertex_table, edge_table = import_model("primitives/Torus.txt")
+    vertex_table, edge_table = import_model("primitives/Dodecahedron.txt")
 
     frame = 0
     refresh_screen()
